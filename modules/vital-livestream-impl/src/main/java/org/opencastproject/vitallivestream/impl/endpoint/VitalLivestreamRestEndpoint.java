5dd4345b678f739737f4fa15ded39988256c40e7
Arnei <arnewilken@yahoo.de>
2024-02-01 10:13:46 +0100

    Add necessary changes to vital paella player
    Turns out we had some changes in Paella6.
    This ports them to the new Paella7 in the vital-paella module.

--- modules/vital-livestream-impl/src/main/java/org/opencastproject/vitallivestream/impl/endpoint/VitalLivestreamRestEndpoint.java
+++ modules/vital-livestream-impl/src/main/java/org/opencastproject/vitallivestream/impl/endpoint/VitalLivestreamRestEndpoint.java
@@ -422,7 +422,9 @@ public class VitalLivestreamRestEndpoint {
             + "\"presenter\":"
             + "\"https://bitdash-a.akamaihd.net/content/MI201109210084_1/"
             + "m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8\","
-            + "\"slides\": \"https://s3.opencast-niedersachsen.de/public/hls-test/720p.m3u8\""
+            + "\"slides\":"
+            + "\"https://bitdash-a.akamaihd.net/content/MI201109210084_1/"
+            + "m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8\""
             + "}}").build();
   }
 
--- modules/vital-paella-player/src/js/PaellaOpencast.js
+++ modules/vital-paella-player/src/js/PaellaOpencast.js
@@ -30,7 +30,7 @@ import getZoomPluginContext from 'paella-zoom-plugin';
 import getUserTrackingPluginContext from 'paella-user-tracking';
 
 import { loadTrimming, setTrimming } from './TrimmingLoader';
-import EpisodeConversor from './EpisodeConversor.js';
+//import EpisodeConversor from './EpisodeConversor.js';
 import packagePom from '../../pom.xml';
 
 import dictionary from '../default-dictionaries.js';
@@ -121,12 +121,61 @@ const initParams = {
 
     // Load episode
     const loadEpisode = async () => {
-      const response = await fetch(url);
+      // VITAL PAELLA PLAYER CHANGES
+      const episodeId = url.split('id=')[1];
+      const response = await fetch('/vital-livestream/streams/' + episodeId);
 
       if (response.ok) {
         const data = await response.json();
-        const conversor = new EpisodeConversor(data, config.opencast || {});
-        return conversor.data;
+        if (data['streams']) {
+
+          var convertedEpisode = [];
+          var mandatoryFlavors = ['presenter', 'presentation'];
+          for (var stream in data['streams']) {
+            // Fix flavor, else the Opemcast-Paella-Config will not display the streams
+            var flavor = '';
+            if (mandatoryFlavors.includes(stream)) {
+              flavor = stream;
+              mandatoryFlavors.splice(mandatoryFlavors.indexOf(flavor), 1);
+            } else {
+              if (mandatoryFlavors.length > 0) {
+                flavor = mandatoryFlavors[0];
+                mandatoryFlavors.splice(0, 1);
+              }
+            }
+
+            // Create stream object
+            var item = {
+              audioTag: undefined,
+              content: flavor,
+              preview: '',
+              type: 'video',
+              sources: {
+                hls: [{
+                  isLiveStream: true,
+                  isMaster: false,
+                  src: data['streams'][stream],
+                  preview: '',
+                  mimetype: 'application/x-mpegURL',
+                }]
+              }
+            };
+            convertedEpisode.push(item);
+          }
+          var fixedData = {
+            streams: convertedEpisode,
+            metadata: {title: 'title'},
+            frameList: [],
+            captions: [],
+          };
+
+          return fixedData;
+        }
+        else {
+          throw Error('VITAL PAELLA: Response did not contain streams');
+        }
+        //        const conversor = new EpisodeConversor(data, config.opencast || {});
+        //        return conversor.data;
       }
       else {
         throw Error('Invalid manifest url');
